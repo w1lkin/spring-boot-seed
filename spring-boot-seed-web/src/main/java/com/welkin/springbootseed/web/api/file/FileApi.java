e68933895f9e3b4ef995f9325a6ec018e7328d49
陈炎坤 <chenyankun2832@tops001.com>
2017-10-12 14:34:55 +0800

    commit

---
+++ spring-boot-seed-web/src/main/java/com/welkin/springbootseed/web/api/file/FileApi.java
@@ -0,0 +1,35 @@
+package com.welkin.springbootseed.web.api.file;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+/**
+ * File
+ * @author welkin
+ */
+@Path("file")
+@Api(tags = "file", description = "file")
+public class FileApi {
+
+  private static final Logger logger = LogManager.getLogger(FileApi.class);
+
+  @GET
+  @Consumes({MediaType.MULTIPART_FORM_DATA})
+  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+  @Path("/upload")
+  @ApiOperation("upload")
+  public void upload(@Multipart List<Attachment> list) {
+
+  }
+}
