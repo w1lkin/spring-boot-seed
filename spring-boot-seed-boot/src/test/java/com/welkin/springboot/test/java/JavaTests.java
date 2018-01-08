fa46a2d73786346ddd84fa264d54aa7b2add0a6e
陈炎坤 <chenyankun2832@tops001.com>
2018-01-08 15:35:59 +0800

    mapTest

---
+++ spring-boot-seed-boot/src/test/java/com/welkin/springboot/test/java/JavaTests.java
@@ -0,0 +1,40 @@
+package com.welkin.springboot.test.java;
+
+import com.alibaba.fastjson.JSON;
+import com.welkin.springboot.test.BaseTests;
+import org.junit.Test;
+
+import java.util.*;
+
+public class JavaTests extends BaseTests {
+
+    @Test
+    public void mapTest(){
+        Map<Integer,Integer> linkedHashMap=new LinkedHashMap();
+        linkedHashMap.put(null,1);
+        linkedHashMap.put(3,1);
+        linkedHashMap.put(2,1);
+        linkedHashMap.put(1,null);
+
+        Map<Integer,Integer> hashMap=new HashMap<>();
+        hashMap.put(null,1);
+        hashMap.put(3,1);
+        hashMap.put(2,1);
+        hashMap.put(1,null);
+
+        Map<Integer,Integer> hashtable=new Hashtable<>();
+//        map2.put(null,1);
+        hashtable.put(3,1);
+        hashtable.put(2,1);
+//        map2.put(1,null);
+
+        Map<Integer,Integer> treeMap=new TreeMap<>();
+//        map3.put(null,1);
+        treeMap.put(3,1);
+        treeMap.put(2,1);
+//        map3.put(1,null);
+
+        System.out.println("linkedHashMap:" + JSON.toJSONString(linkedHashMap)+" hashMap:"+JSON.toJSONString(hashMap)+" hashtable:"+JSON.toJSONString(hashtable)+" treeMap:"+JSON.toJSONString(treeMap));
+    }
+
+}
